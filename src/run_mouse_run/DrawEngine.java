b14946f8e7dcd2bcb50d1486bfbdf8535becab5c
Oussama LightRay <SakasakyRay@Gmail.com>
2017-07-13 14:26:04 +0100

    changed resizeImage method

--- src/run_mouse_run/DrawEngine.java
+++ src/run_mouse_run/DrawEngine.java
@@ -622,48 +622,18 @@ public class DrawEngine {
 
 			}
 
-			public BufferedImage resizeImage(BufferedImage image, int areaWidth, int areaHeight) { /// TODO : shorter function
-				float scaleX = (float) areaWidth / image.getWidth();
-				float scaleY = (float) areaHeight / image.getHeight();
-				float scale = Math.min(scaleX, scaleY);
-				int w = Math.round(image.getWidth() * scale);
-				int h = Math.round(image.getHeight() * scale);
+			public BufferedImage resizeImage(BufferedImage image, int newWidth, int newHeight) {
+				// Get resized image
+				Image img = image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
 
+				// Convert to BufferedImage
+				image = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_BYTE_INDEXED);
 
-				int type = image.getTransparency() == Transparency.OPAQUE ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+				Graphics2D bGr = image.createGraphics();
+				bGr.drawImage(img, 0, 0, null);
+				bGr.dispose();
 
-				boolean scaleDown = scale < 1;
-
-				if (scaleDown)
-				{
-					// multi-pass bilinear div 2
-					int currentW = image.getWidth();
-					int currentH = image.getHeight();
-					BufferedImage resized = image;
-					while (currentW > w || currentH > h)
-					{
-						currentW = Math.max(w, currentW / 2);
-						currentH = Math.max(h, currentH / 2);
-
-						BufferedImage temp = new BufferedImage(currentW, currentH, type);
-						Graphics2D g2 = temp.createGraphics();
-						g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-						g2.drawImage(resized, 0, 0, currentW, currentH, null);
-						g2.dispose();
-						resized = temp;
-					}
-					return resized;
-				} else
-				{
-					Object hint = scale > 2 ? RenderingHints.VALUE_INTERPOLATION_BICUBIC : RenderingHints.VALUE_INTERPOLATION_BILINEAR;
-
-					BufferedImage resized = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-					Graphics2D g2 = resized.createGraphics();
-					g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
-					g2.drawImage(image, 0, 0, w, h, null);
-					g2.dispose();
-					return resized;
-				}
+				return image;
 			}
 
 			@Override
